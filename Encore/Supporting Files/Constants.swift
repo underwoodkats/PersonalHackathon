d5758d1322dbcb050e41609f231958e2fadbf28d
Igor Katselenbogen <underwoodkats@gmail.com>
2020-12-26 16:19:35 +0900

    create constants file

---
+++ Encore/Supporting Files/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Encore
+//
+//  Created by Katselenbogen, Igor on 2020/12/26.
+//
+
+import Foundation
+
+struct K {
+    
+    // Cell Names
+    static let scheduleCellNibName = "ScheduleCell"
+    
+    // Cell Identifiers
+    static let scheduleCellIdentifier = "ScheduleCell"
+}
