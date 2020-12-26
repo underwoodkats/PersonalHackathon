d54efa76b2ac1837c862ff061a32ccc59c54c9bb
Igor Katselenbogen <underwoodkats@gmail.com>
2020-12-26 11:58:56 +0900

    create HomeVC

--- Encore/Controllers/HomeViewController.swift
+++ Encore/Controllers/HomeViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
