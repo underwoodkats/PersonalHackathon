92455782016d8a8ee400f943837e894bee2c56c9
Igor Katselenbogen <underwoodkats@gmail.com>
2020-12-26 16:23:24 +0900

    create ScheduleCell

---
+++ Encore/Views/ScheduleCell.swift
@@ -0,0 +1,31 @@
+//
+//  ScheduleViewCell.swift
+//  Encore
+//
+//  Created by Katselenbogen, Igor | Rogi | MESD on 2020/12/26.
+//
+
+import UIKit
+
+class ScheduleCell: UITableViewCell {
+
+    @IBOutlet weak var stageLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var backgroundGradientView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setGradientView()
+    }
+
+    func setGradientView() {
+        // TODO: Implement gradient
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
