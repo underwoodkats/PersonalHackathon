31462e5ec57ba6944868b9890582cc0ad97cb0c4
Igor Katselenbogen <underwoodkats@gmail.com>
2020-12-27 18:03:45 +0900

    create String Extension for capitalization first letter in a string

---
+++ Encore/Extensions/String+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extension.swift
+//  Encore
+//
+//  Created by Katselenbogen, Igor | Rogi | MESD on 2020/12/27.
+//
+
+import UIKit
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+      return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+      self = self.capitalizingFirstLetter()
+    }
+}
