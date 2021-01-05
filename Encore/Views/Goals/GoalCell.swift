5712b6c2c68b44e61bdd40e9e4d6a0039eda27be
Igor Katselenbogen <underwoodkats@gmail.com>
2021-01-05 21:28:01 +0900

    add option to complete goals for goal cell

--- Encore/Views/Goals/GoalCell.swift
+++ Encore/Views/Goals/GoalCell.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class GoalCell: UITableViewCell {
 
+    // TODO: We need to think how user can correct the goals
+    
     @IBOutlet weak var goalLabel: UILabel!
+    @IBOutlet weak var goalInProcessImage: UIImageView!
+    @IBOutlet weak var goalCompletedImage: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        goalInProcessImage.isHidden = true
+        goalCompletedImage.isHidden = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        // TODO: We need to think how user can correct the goals
         // One of the idea is
     }
 }
