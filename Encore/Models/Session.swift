53b1a4c6cc7350fd9dc2bfcae352c9a32dddd05b
Igor Katselenbogen <underwoodkats@gmail.com>
2021-01-06 12:01:17 +0900

    improve sessions - add counter for focused work, structure todos

--- Encore/Models/Session.swift
+++ Encore/Models/Session.swift
@@ -7,54 +7,48 @@
 
 import Foundation
 
+// TODO: Level 1 - Make hints for each stages
+// TODO: Level 3 - Make Focused Work counter automatic 1/2, 1/3 etc.
+
 struct Session {
     var stages: [Stage]
 }
 
 struct Sessions {
     static let miniSession: Session = Session(stages: [
-        Stage(name: "Session Overiview", durationInSeconds: 10*60),
-        Stage(name: "Focused Work 1", durationInSeconds: 45*60),
+        Stage(name: "Focused Work 1/3", durationInSeconds: 45*60),
         Stage(name: "Active Break", durationInSeconds: 15*60),
-        Stage(name: "Focused Work 2", durationInSeconds: 60*60),
+        Stage(name: "Focused Work 2/3", durationInSeconds: 60*60),
         Stage(name: "Small Meal Break", durationInSeconds: 20*60),
-        Stage(name: "Focused Work 3", durationInSeconds: 60*60),
+        Stage(name: "Focused Work 3/3", durationInSeconds: 60*60),
     ])
     
     static let halfDaySession: Session = Session(stages: [
-        Stage(name: "Session Overiview", durationInSeconds: 15*60),
-        Stage(name: "Focused Work 1", durationInSeconds: 45*60),
+        Stage(name: "Focused Work 1/4", durationInSeconds: 45*60),
         Stage(name: "Meal Break", durationInSeconds: 50*60),
         Stage(name: "Stretching", durationInSeconds: 10*60),
-        Stage(name: "Focused Work 2", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 2/4", durationInSeconds: 90*60),
         Stage(name: "Active Break", durationInSeconds: 20*60),
-        Stage(name: "Focused Work 3", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 3/4", durationInSeconds: 90*60),
         Stage(name: "Focus Exercise", durationInSeconds: 10*60),
         Stage(name: "Active Break", durationInSeconds: 10*60),
-        Stage(name: "Focused Work 4", durationInSeconds: 45*60),
+        Stage(name: "Focused Work 4/4", durationInSeconds: 45*60),
     ])
     
     static let fullDaySession: Session = Session(stages: [
-        Stage(name: "Session Overiview", durationInSeconds: 15*60),
-        Stage(name: "Focused Work 1", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 1/6", durationInSeconds: 90*60),
         Stage(name: "Active Break", durationInSeconds: 15*60),
-        Stage(name: "Focused Work 2", durationInSeconds: 45*60),
+        Stage(name: "Focused Work 2/6", durationInSeconds: 45*60),
         Stage(name: "Meal Break", durationInSeconds: 50*60),
         Stage(name: "Stretching", durationInSeconds: 10*60),
-        Stage(name: "Focused Work 3", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 3/6", durationInSeconds: 90*60),
         Stage(name: "Active Break", durationInSeconds: 20*60),
-        Stage(name: "Focused Work 4", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 4/6", durationInSeconds: 90*60),
         Stage(name: "Focus Exercise", durationInSeconds: 10*60),
         Stage(name: "Active Break", durationInSeconds: 10*60),
-        Stage(name: "Focused Work 5", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 5/6", durationInSeconds: 90*60),
         Stage(name: "Meal Break", durationInSeconds: 50*60),
         Stage(name: "Stretching", durationInSeconds: 10*60),
-        Stage(name: "Focused Work 6", durationInSeconds: 90*60),
+        Stage(name: "Focused Work 6/6", durationInSeconds: 90*60),
     ])
 }
-
-
-// TODO: Think about hint for each stage
-// TODO: Think if we can make a screen for basic recommendations
-
-
